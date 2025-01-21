06b3f7c0ae7b13c8d4ebf9b8a90bd647150846ab
bmolinh <bmolinah@icloud.com>
2025-01-21 00:37:27 -0300

    feat: added sequelize postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeConfigService } from './database/sequelize-config.service';
 
 @Module({
   imports: [
@@ -10,6 +12,9 @@ import configuration from './config/configuration';
       isGlobal: true,
       ignoreEnvFile: true,
       load: [configuration],
+    }),
+    SequelizeModule.forRootAsync({
+      useClass: SequelizeConfigService,
     }),
   ],
   controllers: [AppController],
